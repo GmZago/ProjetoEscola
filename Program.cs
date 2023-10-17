977d0b7df69fa473abf2320867b0d3345b1ed099
GmZago <gm.zago@hotmail.com>
2023-10-16 22:24:45 -0300

    TESTE COMMIT

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//TESTE DE COMMIT
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
