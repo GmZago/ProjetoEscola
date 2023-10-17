5710657dcb84d235ea4e2081582a67e4f01e319b
GmZago <gm.zago@hotmail.com>
2023-10-17 08:32:11 -0300

    tESTE 2

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-//TESTE DE COMMIT BRANCH
+//TESTE DE COMMIT BRANCH JULIO
 
 var app = builder.Build();
 
